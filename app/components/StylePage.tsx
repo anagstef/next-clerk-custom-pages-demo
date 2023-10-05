159853d57629d67073e4945c7c6d1ba5de16a3a9
Stefanos Anagnostou <stefanos@clerk.dev>
2023-10-05 13:44:43 +0300

    Fix css targeting in org

--- app/components/StylePage.tsx
+++ app/components/StylePage.tsx
@@ -2,7 +2,7 @@ export const StylePage = () => {
     return (
         <>
             <h1>Style page</h1>
-            <p className="custom-styled-text">This is a text targeted with<br/> <code>.cl-userProfile-root .custom-styled-text</code></p>
+            <p className="custom-styled-text">This is a text targeted with css from outside. It should be green in UserProfile and red in OrganizationProfile.</p>
         </>
     );
 };
